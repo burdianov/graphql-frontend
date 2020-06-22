6d592a164c134c55ea704a637a58c22cf196fcf6
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-22 17:46:09 +0400

    wrap app in ApolloProvider

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import { client } from './init/client';
 
 export const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <h1>App</h1>
-    </>
+    </ApolloProvider>
   );
 };
