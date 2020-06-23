a515862362a15ea41cb1354a6e580f83f356b46d
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-23 16:55:43 +0400

    configure cache

--- src/app/init/client.js
+++ src/app/init/client.js
@@ -1,8 +1,17 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // GraphQL Server
 const uri = 'https://funded-pet-library.moonhighway.com/';
+const link = createHttpLink({
+  uri
+});
+
+// Cache initialization
+const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-  uri
+  cache,
+  link
 });
